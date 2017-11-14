3bb5e611371dd05a8be11cc768b10b33f649ffdb
veNqus <Azragot@gmail.com>
2017-11-14 21:28:37 +0100

    czesc api

---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+            {
+                if (customer.MembershipTypeId == 1 || customer.MembershipTypeId == 0)
+
+                    return ValidationResult.Success;
+                if (customer.BirthdayDate == null)
+                    return new ValidationResult("Birthdate is required.");
+
+                var age = DateTime.Today.Year - customer.BirthdayDate.Value.Year;
+
+                return (age >= 18)
+                    ? ValidationResult.Success
+                    : new ValidationResult("Customer must be over 18 years old to go on subscription");
+            }
+        }
+    }
+}
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -12,9 +12,16 @@ namespace Vidly.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public Genres Genre { get; set; }
+
         public int GenreId { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Added Date")]
         public DateTime AddingDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
         public int AmountInStock { get; set; }
 
     }
